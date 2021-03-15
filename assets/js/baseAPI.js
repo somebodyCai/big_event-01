bd6f2a1642d9376742a9e50a25d9392429013de5
somebodyCai <714577437@qq.com>
2021-03-15 10:08:08 +0800

    完成首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,8 +6,30 @@ $(function () {
     //  JQ封装的 ajax请求超级过滤器 用于请求前后的参数校验与设置 
     $.ajaxPrefilter(function (option) {
 
-        // 补全请求地址
+        // 1.补全请求地址
         option.url = baseURL + option.url;
+
+        // 2.给 含有 /my/ 的请求地址添加密钥
+        if (option.url.indexOf('/my/') != -1) {
+            // 配置密钥
+            option.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+
+            // 登录拦截
+            option.complete = function (res) {
+
+                let obj = res.responseJSON;
+                // 
+                if (obj.status == 1 && obj.message === '身份认证失败！') {
+                    // 销毁本地储存密钥
+                    localStorage.removeItem('token');
+                    // 跳转到登录页面
+                    location.href='/login.html';
+                }
+            }
+
+        }
     })
 
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,61 @@
 $(function () {
-    
+    // 渲染头像
+    getuserIfo()
+
+
+    // 点击退出登录
+    let layer =layui.layer;
+    $('#btnlogout').on('click',function(){
+
+        layer.confirm('是否确定退出登录？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            // 移除本地密钥
+            localStorage.removeItem('token');
+            // 跳转到登陆页面
+            location.href='/login.html'
+            // 关闭弹出层
+            layer.close(index);
+          });
+
+    })
 })
+
+
+function getuserIfo() {
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        data: {},
+        // 请求密钥配置
+        // headers: { Authorization: localStorage.getItem('token') },
+        success: function (res) {
+            console.log(res);
+            console.log(1);
+            if (res.status != 0) {
+                // 弹出获取失败信息
+                return layui.layer.msg(res.message, { icon: 5 })
+            }
+            // 渲染头像
+            randerAvatar(res.data)
+        }
+    })
+}
+
+
+// 封装渲染头像函数
+
+function randerAvatar(user) {
+
+    let name = user.nickname || user.username;
+
+    $('#welcome').html('欢迎' + name)
+
+    if (!user.user_pic) {
+        $('.text-avatar').show().html(user.username[0].toUpperCase());
+        $('.userifo img').hide();
+    }else {
+        $('.userifo img').show().attr('src',user.user_pic);
+        $('.text-avatar').hide()
+    }
+}
+
